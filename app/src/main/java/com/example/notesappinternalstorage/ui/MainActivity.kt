c14437c5e851d3688a811cf8d72227c7ae3c19a0
Volodymyr Kolomyichuk <wowakolom@gmail.com>
2025-01-23 18:20:30 +0200

    Create base package structure for the project

--- app/src/main/java/com/example/notesappinternalstorage/ui/MainActivity.kt
+++ app/src/main/java/com/example/notesappinternalstorage/ui/MainActivity.kt
@@ -1,10 +1,7 @@
-package com.example.notesappinternalstorage
+package com.example.notesappinternalstorage.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.notesappinternalstorage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
